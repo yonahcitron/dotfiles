f3fb72716d6dff3595391701dc1baea62f53b847
yonahcitron <citronyonah@gmail.com>
2025-03-12 18:07:37 +0000

    Added a tools directory that is symlinked to my home directory.

--- arch-setup.sh
+++ arch-setup.sh
@@ -145,27 +145,21 @@ else
 fi
 
 
-HOME_STOW_DIR=$dotfiles/user_configs
-GLOBAL_STOW_DIR=$dotfiles/global_configs
-HOME_TARGET="/home/$USER_ACCOUNT"
-GLOBAL_TARGET="/etc"
-
-echo "The pwd is: $WORKING_DIR"
-echo "The home stow dir is: $HOME_STOW_DIR"
-echo "The global stow dir is: $GLOBAL_STOW_DIR"
-echo "The home target is: $HOME_TARGET"
-echo "The global target is: $GLOBAL_TARGET"
-
 # In order to get the intended functionality of treating each of the subfolders of the stow dir as a module, and reacreate each of their substructures within the target dirs, rather than just dumping them in the target dir directly, we *must* use the cd approach. This is why we can specify the --dir directly for our command which would be more elegant.
-echo "Setting up user configs." 
-cd $HOME_STOW_DIR
-stow --target $HOME_TARGET */  # User configs.
+echo "Setting up Yonah's user configs." 
+cd $dotfiles/user_configs
+stow --target /home/$USER_ACCOUNT */  # User configs.
 
 # Global configs.
-echo "Setting up global configs." 
-cd $GLOBAL_STOW_DIR
-sudo stow --target $GLOBAL_TARGET */
+echo "Setting up Yonah's global configs." 
+cd $dotfiles/global_configs
+sudo stow --target /etc */
 
+# Tools.
+echo "Setting up Yonah's cmdlet tools."
+cd $dotfiles
+mkdir -p tools
+stow --target $HOME_TARGET/tools tools
 cd $WORKING_DIR
 
 
