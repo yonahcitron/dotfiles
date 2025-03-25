c4c6adb20e3034a79450c162426293450926d027
Yonah Citron <citronyonah@gmail.com>
2025-03-25 10:56:20 +0000

    Added yazi configs
    Also added to arch installation script

--- install/create_custom_iso.sh
+++ install/create_custom_iso.sh
@@ -2,7 +2,7 @@
 # TODO: See if there's a way to have both line numbers at the same time
 
 # Local paths
-install_dir="$dotfiles/install/"
+install_dir="$dotfiles/install"
 iso_name="archlinux-x86_64.iso"
 cache_dir="$install_dir/.cache"
 iso_path="$cache_dir/$iso_name"
@@ -12,10 +12,24 @@ build_dir="$install_dir/.build"
 if [ ! -e $iso_path ]; then
   mkdir -p $iso_dir
   wget -O $iso_path "https://london.mirror.pkgbuild.com/iso/latest/$iso_name"
+  echo "Downloaded Arch ISO file to $iso_path."
 else
   echo "Arch ISO file found locally, skipping download."
 fi
 
-# Copy the ISO to the build directory.
-mkdir -p $build_dir
-cp $iso_path $build_dir
+# Extract the ISO to the build directory.
+# USE SOME OTHER METHOD TO DO THIS..
+# mkdir -p $cache_dir/iso_read_only_mount
+# Mount the iso, copy to the build directory, and unmount.
+# mkdir -p $build_dir/extracted_iso
+# sudo mount -o loop $iso_path $cache_dir/iso_read_only_mount
+# sudo cp -r $cache_dir/iso_read_only_mount $build_dir/extracted_iso
+# sudo umount $cache_dir/iso_read_only_mount
+# rm -rf $cache_dir/iso_read_only_mount
+
+# Use the other method instead... maybe 7z
+
+# Put the final ISO in the install directory.
+custom_iso_name="yonah-$iso_name"
+
+# Delete the build artifacts.
