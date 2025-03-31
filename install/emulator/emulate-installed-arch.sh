f55582c40b4150ea3fd2005da141db41d8427cc6
Yonah Citron <citronyonah@gmail.com>
2025-03-31 16:35:28 +0100

    Added some debugging lines.

--- install/emulator/emulate-installed-arch.sh
+++ install/emulator/emulate-installed-arch.sh
@@ -1,21 +1,36 @@
-iso_name="yonah-custom-archlinux-auto-installer-x86_64.iso"
-iso_path="$iso_dir/$iso_name"
-disk_name="arch-disk.img"
 emulator_dir="$dotfiles/install/emulator"
+
+disk_name="arch-disk.img"
 disk_dir="$emulator_dir/disk"
 disk_path="$disk_dir/$disk_name"
-nvram_path="$emulator_dir/nvram"
+
+nvram_dir="$emulator_dir/nvram"
 nvram_name="OVFM_VARS.fd"
+nvram_path="$nvram_dir/$nvram_name"
+
+# Check if the paths exist and exit if not
+if [ ! -e "$disk_path" ]; then
+  echo "No disk image found at $disk_path, aborting."
+  exit 1
+else
+  echo "Disk image found at $disk_path, loading existing."
+fi
+if [ ! -e "$nvram_path" ]; then
+  echo "No nvram file found at $nvram_path, aborting."
+  exit 1
+else
+  echo "NVRAM file found at $nvram_path, loading existing."
+fi
 
 qemu-system-x86_64 \
   -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
-  -drive if=pflash,format=raw,file="$nvram_path/$nvram_name" \
+  -drive if=pflash,format=raw,file="$nvram_path" \
   -enable-kvm \
   -cpu host \
   -m 2048 \
   -machine q35 \
   -drive file=$disk_path,format=qcow2,if=virtio \
-  -boot menu=on
+  -boot order=c,menu=on
 
 # TODO: MAKE THE VNC VIEWER ALSO POP UP WHEN I RUN THIS!!!!
 # ALSO FIND OUT WHY GIT ISN'T INSTALLED ALREADY ON THE INSTALLED MACHINE TO DO A GIT CLONE ETC...
