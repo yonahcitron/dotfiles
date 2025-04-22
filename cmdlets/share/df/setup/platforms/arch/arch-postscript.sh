81334d654b5ad3c2a525f31fcf17c7567b21b894
Yonah Citron <citronyonah@gmail.com>
2025-04-22 08:48:21 +0100

    Migrated kmonad -> interception tools for greater flexibility. Remapped caps lock.
    Caps lock has multi-functionality ... esc, meta key, and caps

--- cmdlets/share/df/setup/platforms/arch/arch-postscript.sh
+++ cmdlets/share/df/setup/platforms/arch/arch-postscript.sh
@@ -4,16 +4,13 @@
 
 sudo systemctl daemon-reload
 
-# Using a template service to enable multiple kmonad services for each device.
-# TODO: Use conditional per-device logic here to start the correct services.
-# REMEMBER: When enabling new remappings, be sure to test them first just using the kmonad cli, and only THEN add them as a service to make sure they work! Will save me time in the long run.
-sudo systemctl enable kmonad@thinkpad-keyboard-remap.service
+sudo systemctl enable udevmon.service
 systemctl --user enable pipewire pipewire-pulse wireplumber
 
 if ! systemctl --quiet is-system-running; then
   echo "In chroot — skipping service start"
 else
   echo "Starting services..."
-  sudo systemctl start kmonad@thinkpad-keyboard-remap.service
+  sudo systemctl start udevmon.service
   systemctl --user start pipewire pipewire-pulse wireplumber
 fi
