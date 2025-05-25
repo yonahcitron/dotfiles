75762e64469b2ca435ca2de21d69e961af2354da
Yonah Citron <citronyonah@gmail.com>
2025-05-25 12:46:49 +0100

    Updated workspaces to autosave

--- user_configs/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ user_configs/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,8 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      bind_to_cwd = true,
+      follow_current_file = { enabled = false },
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
--- user_configs/nvim/.config/nvim/lua/plugins/workspaces.lua
+++ user_configs/nvim/.config/nvim/lua/plugins/workspaces.lua
@@ -1,12 +1,19 @@
 -- I've configured workspaces.nvim to auto-save per workspace if it detect the relevant marker file, see hooks.
-
+local autosave = require("auto-save")
 return {
   "natecraddock/workspaces.nvim",
   opts = {
     hooks = {
       open = {
-        function()
-          vim.notify("workspace opened!!!")
+        function(name, path)
+          local marker = path .. "/.nvim-workspace/.autosave"
+          if vim.fn.filereadable(marker) == 1 then
+            autosave.on()
+            vim.notify("workspace:" .. name .. "\n" .. "autosave:" .. "enabled")
+          else
+            autosave.off()
+            vim.notify("workspace:" .. name .. "\n" .. "autosave:" .. "disabled")
+          end
         end,
       },
     },
