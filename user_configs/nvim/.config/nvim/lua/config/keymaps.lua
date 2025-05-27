902c23dc0f66d458e04da3332e5049f89ab7402b
Yonah Citron <citronyonah@gmail.com>
2025-05-27 17:17:44 +0100

    Added back ; -> : remap for easier firing of commands in nvim

--- user_configs/nvim/.config/nvim/lua/config/keymaps.lua
+++ user_configs/nvim/.config/nvim/lua/config/keymaps.lua
@@ -17,3 +17,8 @@ vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
 vim.keymap.set("n", "<C-\\>", "<Cmd>TmuxNavigatePrevious<CR>", { silent = true })
+
+-- Swap `:` and `;` keys to make entering Command Mode easier
+-- Also swaps command to repeat the last 'f', 't', 'F' & 'T' command.
+vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
+vim.api.nvim_set_keymap("n", ":", ";", { noremap = true })
--- user_configs/vim/.vimrc
+++ user_configs/vim/.vimrc
@@ -5,6 +5,11 @@ set number
 inoremap jk <Esc>
 inoremap kj <Esc>
 
+" Swap `:` and `;` keys to make entering Command Mode easier.
+"	Also swaps command to repeat the last 'f', 't', 'F' & 'T' command.
+nnoremap ; :
+nnoremap : ;
+
 " Saner default to navigate windows with Ctrl + h/j/k/l
 nnoremap <C-h> <C-w>h
 nnoremap <C-j> <C-w>j
