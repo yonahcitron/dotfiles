c67f44aba8eb2bb0909eaac6b16cc0394360594b
Yonah Citron <citronyonah@gmail.com>
2025-05-28 16:23:01 +0100

    Added some plugins, especially for sessions

--- cmdlets/share/df/setup/platforms/mac/devices/Q2QK9J44K7/Q2QK9J44K7-init.sh
+++ cmdlets/share/df/setup/platforms/mac/devices/Q2QK9J44K7/Q2QK9J44K7-init.sh
@@ -1,2 +1,3 @@
 brew tap databricks/tap
 xargs brew install <$DF_DEVICE_BREW_PACKAGES # For apps etc I only need on my work laptop
+pip3 install jupytext                        # To allow me to view the databricks source code.
--- cmdlets/share/df/setup/platforms/mac/devices/Q2QK9J44K7/Q2QK9J44K7.zprofile
+++
@@ -1,2 +0,0 @@
-# Zscaler gets in the way of loads of things. Kill it at startup on work mac.
- ps aux | grep -i zscaler | awk '{print $2}' | xargs sudo kill -9
